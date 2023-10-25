14f7cabf929caf6c42e6c2862df5afc395375237
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-10-25 10:41:15 -0400

    Programmatically navigating to the edit page

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipes-detail',
@@ -13,6 +13,7 @@ export class RecipesDetailComponent implements OnInit{
   id: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -27,5 +28,12 @@ export class RecipesDetailComponent implements OnInit{
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  };
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route})
+
+    //Another way but can be a bit complicated
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
   }
 }
